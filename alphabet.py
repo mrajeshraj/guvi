34e07d2f588f8dbfeaaa68da2d84aa57f65e9462
mrajeshraj <50876303+mrajeshraj@users.noreply.github.com>
2019-05-27 22:59:04 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,6 @@
-def find_variable(ch):
-  if(ch=='a' or ch=='b' or ch=='c' or ch=='d'):
-    print("Alphabet")
-  else:
-    print("Not an Alphabet")
-variable=find_variable('a')    
+ch=input()
+if(ch=='a' or ch=='b' or ch=='c' or ch=='d'):
+  print("Alphabet")
+else:
+  print("Not an Alphabet")
+
